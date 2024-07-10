692ffd02be5d4c215bba552b0cdf33bdc643d914
mlobf <mlob>
2024-07-10 09:30:07 -0300

    error handling

---
+++ main/6_error_handling/aux/unknown_error.py
@@ -0,0 +1,8 @@
+"""unknown error """
+
+
+def unknown_error(people: list[str]):
+    """How to fix an unknown_error"""
+    for p in people:
+        print(type(p))
+        print(p)#Do automatic type convertion
--- main/6_error_handling/main.py
+++ main/6_error_handling/main.py
@@ -3,12 +3,24 @@
 
 from aux.else_finally import else_finally
 from aux.raise_statement import raise_statement, is_connected
+from aux.unknown_error import unknown_error
 
 
 if __name__ == "__main__":
     # else_finally()
     # raise_statement()
+    """
     try:
         is_connected(connection=True)
     except ConnectionError:
         print("There is no connection.")
+
+    """
+    #unknown_error(range(100))
+    unknown_error(['marcos','carlos'])
+
+
+
+
+
+
