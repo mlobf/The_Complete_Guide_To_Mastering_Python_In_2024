77346851060a055e57949deafff7bc2cc6612a7e
mlobf <marcoslemeborbafilho@gmail.com>
2024-07-13 10:49:05 -0300

    Class OOP getter setter

--- main/10_oop/aux/base_classes.py
+++ main/10_oop/aux/base_classes.py
@@ -30,3 +30,32 @@ class Animal:
     def make_trick(self, trick_name: str):
         """Simple trick"""
         self.tricks.append(trick_name)
+
+    def say_hi(self):
+        """Simple say hi"""
+        print("Say hi")
+
+
+class Fruit:
+    """Simple Fruit Class"""
+
+    def __init__(self, name: str,calories:float):
+        """_name is a private variable"""
+        self._name = name
+        self._calories=calories
+
+    @property
+    def name(self):
+        print(f"{self._name}is being accessed")
+        return self._name
+
+    @name.setter
+    def name(self,value):
+        print(f"{self._name}is now {value}")
+        self._name = value
+        return self._name
+
+
+
+
+
--- main/10_oop/main.py
+++ main/10_oop/main.py
@@ -1,24 +1,36 @@
 """Uso de classes
 """
 
-from aux.base_classes import Lamp,Animal
+from aux.base_classes import Lamp, Animal,Fruit
 
 
 if __name__ == "__main__":
-
-    """
+    print('-'*79)
     aladim = Lamp("Led", "Vermelha")
     aladim.turn_on()
     aladim.turn_off()
     aladim.describe()
-    """
-
-    cat = Animal('Tiao')
-    dog = Animal('Dog')
-    cat.make_trick('Dar a pata')
-    cat.make_trick('Comer pate')
-    cat.make_trick('Dormir no sofa tiao')
-    dog.make_trick('Dormir no sofa dog')
-    cat.make_trick('Brincar com o fio')
+    print('-'*79)
+    cat = Animal("Tiao")
+    dog = Animal("Dog")
+    print('-'*79)
+    cat.make_trick("Dar a pata")
+    cat.make_trick("Comer pate")
+    cat.make_trick("Dormir no sofa tiao")
+    dog.make_trick("Dormir no sofa dog")
+    cat.make_trick("Brincar com o fio")
+    print('-'*79)
     print(cat.tricks)
     print(dog.tricks)
+    # Ambas as instancias dividem a lista da variavel  de Classe
+    print('-'*79)
+    cat.name = "Miau"
+    print(cat.name)
+    print('-'*79)
+    banana = Fruit("banana",20.0)
+    print(banana.name)
+    print(banana._calories)
+    banana.name = "mela"
+
+
+
