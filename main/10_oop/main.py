6bc6e40a78a4dfe6778dfa12b1d6ff08823fcdb7
mlobf <marcoslemeborbafilho@gmail.com>
2024-07-13 00:21:00 -0300

    Class Variables Better Comments

--- main/10_oop/main.py
+++ main/10_oop/main.py
@@ -14,8 +14,11 @@ if __name__ == "__main__":
     """
 
     cat = Animal('Tiao')
+    dog = Animal('Dog')
     cat.make_trick('Dar a pata')
     cat.make_trick('Comer pate')
-    cat.make_trick('Dormir no sofa')
+    cat.make_trick('Dormir no sofa tiao')
+    dog.make_trick('Dormir no sofa dog')
     cat.make_trick('Brincar com o fio')
     print(cat.tricks)
+    print(dog.tricks)
