a7335863c1570c38ce587cb1815bbd90d6e7b78d
mlobf <marcoslemeborbafilho@gmail.com>
2024-07-12 23:28:18 -0300

    finish lists

--- main/9_lists/main.py
+++ main/9_lists/main.py
@@ -9,8 +9,8 @@ from aux.teste_list_c import teste_list, slicing_lists
 
 
 if __name__ == "__main__":
-    # print(list_comprehentions_old())
-    # print(list_comprehentions_new())
-    # print(list_comprehentions_captitalize(["marico", "luigi", "peach"]))
-    # print(teste_list())
+    print(list_comprehentions_old())
+    print(list_comprehentions_new())
+    print(list_comprehentions_captitalize(["marico", "luigi", "peach"]))
+    print(teste_list())
     slicing_lists()
